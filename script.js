db327f062674f8aebf6513d342e9872231dbe222
shebiraaja <71530161+shebiraaja@users.noreply.github.com>
2020-11-20 21:22:25 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const menuIcon = document.querySelector(".hamburger-menu");
-const navbar = document.querySelector(".navbar");
+const navlist = document.querySelector(".nav-list");
 
-menuIcon.addEventListener("click", () => {
-    navbar.classList.toggle("change");
-});
+menuIcon.addEventListener("click", (e) => {
+    navlist.classList.toggle("change");
+})
