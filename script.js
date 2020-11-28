021843fcb5286701b8fe03286b202f532a4d3d28
shebiraaja <71530161+shebiraaja@users.noreply.github.com>
2020-11-28 19:54:01 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,60 +1,64 @@
 var span = document.getElementsByTagName("span");
 var div = document.getElementsByTagName("div");
-var l = 0;
+
+var l=0;
+
 span[1].onclick = () => {
     l++;
     for(var i of div){
-        if(l==0) {
+        if(l==0){
             i.style.left = "0px";
         }
 
-        if(l==1) {
+        if(l==1){
             i.style.left = "-740px";
         }
 
-        if(l==2) {
+        if(l==2){
             i.style.left = "-1480px";
         }
 
-        if(l==3) {
+        if(l==3){
             i.style.left = "-2220px";
         }
 
-        if(l==4) {
-            i.style.left = "-2960px";
+        if(l==4){
+            i.style.left = "-2990px";
         }
 
-        if(l>4) {
-            l=4;
+        if(l>4){
+            l=4
         }
+
     }
 
     span[0].onclick = () => {
-        l--; 
+        l--;
         for(var i of div){
-            if(l==0) {
+            if(l==0){
                 i.style.left = "0px";
             }
     
-            if(l==1) {
+            if(l==1){
                 i.style.left = "-740px";
             }
     
-            if(l==2) {
+            if(l==2){
                 i.style.left = "-1480px";
             }
     
-            if(l==3) {
+            if(l==3){
                 i.style.left = "-2220px";
             }
     
-            if(l==4) {
+            if(l==4){
                 i.style.left = "-2960px";
             }
     
-            if(l<0) {
-                l=0;
+            if(l<0){
+                l=0
             }
+    
         }
     }
 }
