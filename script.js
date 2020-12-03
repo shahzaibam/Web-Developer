5b74b8a90d0f2467740dac469604ed70ee608b5c
shebiraaja <71530161+shebiraaja@users.noreply.github.com>
2020-12-03 18:27:50 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,47 +1,64 @@
-//function obtenerListaUsuarios(){
-    //var listaUsuarios = JSON.parse(localStorage.getItem("ListaUsuarios"));
+var span = document.getElementsByTagName("span");
+var div = document.getElementsByTagName("div");
 
-            //if(listaUsuarios === null){
-                listaUsuarios = 
-                [
-                    ["1", "Pabs", "Monestel", "pablo.monestel@gmail.com", "1pablo9", "1987-10-12", "1"],
+var l=0;
 
-                    ["2", "Shebi","Asghar Munir", "shebbi4597@gmail.com", "pakistan92", "2004-30-08", "2"]
-
-                ]
-            //}
-    //return listaUsuarios;
-//}
-
-function validarCredenciales(pCorreo , pContrasenna){
-    //var listaUsuarios = obtenerListaUsuarios();
-    var bAcceso = false;
-
-    for (var i = 0; i < listaUsuarios.length; i++){
-        if(pCorreo == listaUsuarios[i][3] && pContrasenna == listaUsuarios[i][4]){
-            bAcceso = true;
-            //sessionStorage.setItem("usuarioActivo", listaUsuarios[i][1] + " " + listaUsuarios[i][2]);
-            //sessionStorage.setItem("rolUsuarioActivo", listaUsuarios[i][6]);
+span[1].onclick = () => {
+    l++;
+    for(var i of div){
+        if(l==0){
+            i.style.left = "0px";
         }
+
+        if(l==1){
+            i.style.left = "-740px";
+        }
+
+        if(l==2){
+            i.style.left = "-1480px";
+        }
+
+        if(l==3){
+            i.style.left = "-2220px";
+        }
+
+       // if(l==4){
+         //   i.style.left = "-2990px";
+        //}
+
+        if(l>3){
+            l=3
+        }
+
     }
 
-    return bAcceso;
+    span[0].onclick = () => {
+        l--;
+        for(var i of div){
+            if(l==0){
+                i.style.left = "0px";
+            }
+    
+            if(l==1){
+                i.style.left = "-740px";
+            }
+    
+            if(l==2){
+                i.style.left = "-1480px";
+            }
+    
+            if(l==3){
+                i.style.left = "-2220px";
+            }
+    
+            //if(l==4){
+              //  i.style.left = "-2960px";
+            //}
+    
+            if(l<0){
+                l=0
+            }
+    
+        }
+    }
 }
-
-
-function iniciarSesion(){
-    var sCorreo = "";
-    var sContrasenna = "";
-    var bAcceso = false;
-
-    sCorreo = document.querySelector("#txtCorreo").value
-    sContrasenna = document.querySelector("#txtContrasenna").value
-
-    bAcceso = validarCredenciales(sCorreo, sContrasenna);
-    console.log(bAcceso);
-}
-
- 
-document.querySelector("#btnIngresar").addEventListener("click", iniciarSesion);
-
-
