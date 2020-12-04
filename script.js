f8aefcf83fd6d8f6a0c287ffdd3c1b5aeab75601
shebiraaja <71530161+shebiraaja@users.noreply.github.com>
2020-12-04 18:20:58 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,47 @@
-function light(value){
-    var pic;
-    if(value === 0){
-        pic = src = "/img/2020-11-26_223847.jpg"
+//function obtenerListaUsuarios(){
+    //var listaUsuarios = JSON.parse(localStorage.getItem("ListaUsuarios"));
+
+            //if(listaUsuarios === null){
+                listaUsuarios = 
+                [
+                    ["1", "Pabs", "Monestel", "pablo.monestel@gmail.com", "1pablo9", "1987-10-12", "1"],
+
+                    ["2", "Shebi","Asghar Munir", "shebbi4597@gmail.com", "pakistan92", "2004-30-08", "2"]
+
+                ]
+            //}
+    //return listaUsuarios;
+//}
+
+function validarCredenciales(pCorreo , pContrasenna){
+    //var listaUsuarios = obtenerListaUsuarios();
+    var bAcceso = false;
+
+    for (var i = 0; i < listaUsuarios.length; i++){
+        if(pCorreo == listaUsuarios[i][3] && pContrasenna == listaUsuarios[i][4]){
+            bAcceso = true;
+            //sessionStorage.setItem("usuarioActivo", listaUsuarios[i][1] + " " + listaUsuarios[i][2]);
+            //sessionStorage.setItem("rolUsuarioActivo", listaUsuarios[i][6]);
+        }
     }
-    else{
-        pic = src = "/img/bulb on.jpg"
-    }
-    document.getElementById("bulb").src = pic;
+
+    return bAcceso;
 }
+
+
+function iniciarSesion(){
+    var sCorreo = "";
+    var sContrasenna = "";
+    var bAcceso = false;
+
+    sCorreo = document.querySelector("#txtCorreo").value
+    sContrasenna = document.querySelector("#txtContrasenna").value
+
+    bAcceso = validarCredenciales(sCorreo, sContrasenna);
+    console.log(bAcceso);
+}
+
+ 
+document.querySelector("#btnIngresar").addEventListener("click", iniciarSesion);
+
+
