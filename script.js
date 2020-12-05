b11eb3912efe6480e0d99dd1b72072a0f9a888f3
shebiraaja <71530161+shebiraaja@users.noreply.github.com>
2020-12-05 16:56:00 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
-const name = document.getElementById("name");
-const password = document.getElementById("password");
-const form = document.getElementById("form");
-const errorElement = document.getElementById("error");
+const names = document.querySelector("#name");
+const password = document.querySelector("#password");
+const form = document.querySelector("#form");
+const errorElement = document.querySelector("#error");
 
-form.addEventListener("click", (e) => {
+
+form.addEventListener("submit", (e) => {
     let messages = []
-    if (name.value === "" || name.value == null){
-        messages.push("Name is required")
+    if(names.value === "" || names.value === null){
+        messages.push("name is required")
     }
 
-
-    if(password.value.length <= 6) {
-        messages.push("Password must be longer than 6 characters")
+    if(password.value.length <= 6){
+        messages.push("password must be longer than 6 characters")
     }
 
-    if(password.value.length >= 6) {
-        messages.push("Password must be less than 6 characters")
+    if(password.value.length >= 20){
+        messages.push("password must be less than 20 characters")
     }
 
     if(password.value === "password"){
-        messages.push("Password cannot be password")
+        messages.push("password cannot be password")
     }
 
-    if(messages.lenght > 0){
+    if(messages.length > 0){
         e.preventDefault()
         errorElement.innerText = messages.join(", ")
     }
