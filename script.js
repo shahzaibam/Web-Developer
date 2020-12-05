0414d352062ca0fa7a70d9f0093e665a770221ee
shebiraaja <71530161+shebiraaja@users.noreply.github.com>
2020-12-05 16:55:06 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,47 +1,28 @@
-//function obtenerListaUsuarios(){
-    //var listaUsuarios = JSON.parse(localStorage.getItem("ListaUsuarios"));
+const names = document.getElementById("name");
+const password = document.getElementById("password");
+const form = document.getElementById("form");
+const errorElement = document.getElementById("error")
 
-            //if(listaUsuarios === null){
-                listaUsuarios = 
-                [
-                    ["1", "Pabs", "Monestel", "pablo.monestel@gmail.com", "1pablo9", "1987-10-12", "1"],
-
-                    ["2", "Shebi","Asghar Munir", "shebbi4597@gmail.com", "pakistan92", "2004-30-08", "2"]
-
-                ]
-            //}
-    //return listaUsuarios;
-//}
-
-function validarCredenciales(pCorreo , pContrasenna){
-    //var listaUsuarios = obtenerListaUsuarios();
-    var bAcceso = false;
-
-    for (var i = 0; i < listaUsuarios.length; i++){
-        if(pCorreo == listaUsuarios[i][3] && pContrasenna == listaUsuarios[i][4]){
-            bAcceso = true;
-            //sessionStorage.setItem("usuarioActivo", listaUsuarios[i][1] + " " + listaUsuarios[i][2]);
-            //sessionStorage.setItem("rolUsuarioActivo", listaUsuarios[i][6]);
-        }
+form.addEventListener("submit", (e) => {
+    let messages = []
+    if (names.value === "" || names.value == null){
+        messages.push("Name is required")
     }
 
-    return bAcceso;
-}
+    if(password.value.length <= 6) {
+        messages.push("Password must be longer than 6 characters")
+    }
 
+    if(password.value.length >= 20) {
+        messages.push("Password must be less than 6 characters")
+    }
 
-function iniciarSesion(){
-    var sCorreo = "";
-    var sContrasenna = "";
-    var bAcceso = false;
+    if(password.value === "password"){
+        messages.push("Password cannot be password")
+    }
 
-    sCorreo = document.querySelector("#txtCorreo").value
-    sContrasenna = document.querySelector("#txtContrasenna").value
-
-    bAcceso = validarCredenciales(sCorreo, sContrasenna);
-    console.log(bAcceso);
-}
-
- 
-document.querySelector("#btnIngresar").addEventListener("click", iniciarSesion);
-
-
+    if(messages.lenght > 0){
+        e.preventDefault()
+        errorElement.innerText = messages.join(", ")
+    }
+})
