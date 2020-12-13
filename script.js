77681551c81b773d3342f23369d577e41ac764cd
shebiraaja <71530161+shebiraaja@users.noreply.github.com>
2020-12-13 13:42:34 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-let formData;
 
-const signUp = function (e) { 
-    console.log("hi")
-     formData = {
+const signUp = function(e){
+    let formData = {
         fname : document.getElementById("fname").value,
         lname : document.getElementById("lname").value,
         email : document.getElementById("email").value,
@@ -11,50 +9,5 @@ const signUp = function (e) {
     localStorage.setItem("formData", JSON.stringify(formData));
     console.log(localStorage.getItem("formData"));
     
-    e.preventDefault();
-    
+    e.preventDefault()
 }
-var j = localStorage.getItem("formData")
-
-//console.log(signUp)  
-
-
-// let formData = {
-//     fname : "first name",
-//     lname : "last name",
-//     email : "email",
-//     pwd : "pasword"
-//     }
-// localStorage.setItem("formData", JSON.stringify(formData));
-// // console.log(localStorage.getItem("formData"));
-
-// var a =""
-// var b =""
-// var c =""
-// var d = ""
-
-
-// const signUp = function (e) { 
-//     console.log("hi")
-//     a = document.getElementById("fname").value,
-//     b = document.getElementById("lname").value,
-//     c = document.getElementById("email").value,
-//     d = document.getElementById("pwd").value
-    
-//     formData = {
-//                 fname : document.getElementById("fname").value,
-//                 lname : document.getElementById("lname").value,
-//                 email : document.getElementById("email").value,
-//                 pwd : document.getElementById("pwd").value
-//             }
-        
-
-//     e.preventDefault();
-//     localStorage.setItem("formData", JSON.stringify(formData));
-    
-// }
-
-// console.log(a);
-//         console.log(b);
-//         console.log(c);
-//         console.log(d);
