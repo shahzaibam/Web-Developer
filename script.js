74aa7a46725153c2160b6b4af487551439d498f3
shebiraaja <71530161+shebiraaja@users.noreply.github.com>
2020-12-21 18:12:12 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,66 +1,31 @@
-const signUp = function (e) { 
+const signUp = function (e) {
     let formData = {
-        fname : document.getElementById("fname").value,
-        lname : document.getElementById("lname").value,
-        email : document.getElementById("email").value,
-        pwd : document.getElementById("pwd").value
+    fname : document.getElementById("fname").value,
+    lname : document.getElementById("lname").value,
+    email : document.getElementById("email").value,
+    pwd : document.getElementById("pwd")
     }
+
     localStorage.setItem("formData", JSON.stringify(formData));
     console.log(localStorage.getItem("formData"));
 
     e.preventDefault();
-    
+
 }
 
-//////////////////////////////////////sign in
+
+/////////////////////sign in
 
 
 localStorage.getItem("formData")
-
-
 
 function dos (sCorreo, sContrasenna){
 
     const data = JSON.parse(localStorage.getItem("formData"))
 
-    if(data.email === sCorreo && data.pwd == sContrasenna){
-        console.log("bien")
+    if(data.email === sCorreo && data.pwd === sContrasenna){
+        console.log("bien")   
     } else{
-        console.log("nada")
+        console.log("mal")
     }
 }
-
-
-
-/*function validarCredenciales(pCorreo , pContrasenna){
-var bAcceso = false;
-
-    for (var i = 0; i < localStorage.getItem("formData"); i++){
-        if(pCorreo == localStorage.getItem("formData")[i][2] &&    
-        pContrasenna == localStorage.getItem("formData")[i][3]){
-          bAcceso = true;
-        }
-        else{
-            bAcceso = false;
-        }
-    }
-
-    console.log(alert)
-}*/
-
-
-function iniciarSesion(){
-    var sCorreo = "";
-    var sContrasenna = "";
-    var bAcceso;
-
-    sCorreo = document.querySelector("#txtCorreo").value
-    sContrasenna = document.querySelector("#txtContrasenna").value
-
-    bAcceso = dos(sCorreo, sContrasenna);
-    console.log(bAcceso);
-}
-
-
-document.querySelector("#btnIngresar").addEventListener("click", iniciarSesion);
-
