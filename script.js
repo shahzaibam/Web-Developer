b44262eef04004b6d4610ebf7c77adfb01e9b4aa
shebiraaja <71530161+shebiraaja@users.noreply.github.com>
2020-12-22 12:30:54 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,31 +1,49 @@
-const signUp = function (e) {
+const signUp = function (e) { 
     let formData = {
-    fname : document.getElementById("fname").value,
-    lname : document.getElementById("lname").value,
-    email : document.getElementById("email").value,
-    pwd : document.getElementById("pwd")
+        fname : document.getElementById("fname").value,
+        lname : document.getElementById("lname").value,
+        email : document.getElementById("email").value,
+        pwd : document.getElementById("pwd").value
     }
-
     localStorage.setItem("formData", JSON.stringify(formData));
     console.log(localStorage.getItem("formData"));
 
     e.preventDefault();
-
+    
 }
 
-
-/////////////////////sign in
+////////sign in
 
 
 localStorage.getItem("formData")
+
+
 
 function dos (sCorreo, sContrasenna){
 
     const data = JSON.parse(localStorage.getItem("formData"))
 
-    if(data.email === sCorreo && data.pwd === sContrasenna){
-        console.log("bien")   
-    } else{
-        console.log("mal")
+    if(data.email === sCorreo && data.pwd == sContrasenna){
+        console.log("bien")
+    }else{
+        console.log("nada")
     }
 }
+
+
+
+function iniciarSesion(){
+    var sCorreo = "";
+    var sContrasenna = "";
+    var bAcceso;
+
+    sCorreo = document.querySelector("#txtCorreo").value
+    sContrasenna = document.querySelector("#txtContrasenna").value
+
+    bAcceso = dos(sCorreo, sContrasenna);
+    console.log(bAcceso);
+}
+
+
+document.querySelector("#btnIngresar").addEventListener("click", iniciarSesion);
+
