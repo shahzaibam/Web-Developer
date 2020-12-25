513722581704e75cda9dfee4bd983b17ca2c33ac
shebiraaja <71530161+shebiraaja@users.noreply.github.com>
2020-12-25 19:19:53 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,68 @@
-window.addEventListener("scroll", function(){
-    const header = document.querySelector("header");
-    header.classList.toggle("sticky", window.scrollY > 0);
-});
+const menuIcon = document.querySelector(".hamburger-menu");
+const navlist = document.querySelector(".nav-list");
+
+menuIcon.addEventListener("click", (e) => {
+    navlist.classList.toggle("change")
+})
+
+//Code for slider
+
+var counter = 1;
+setInterval(function(){
+    document.getElementById("radio" + counter).checked = true;
+    counter++;
+    if(counter>4){
+        counter = 1;
+    }
+}, 3000);
+
+//Code for SignUp and SignIn
+const signUp = function (e) { 
+    let formData = {
+        fname : document.getElementById("fname").value,
+        lname : document.getElementById("lname").value,
+        email : document.getElementById("email").value,
+        pwd : document.getElementById("pwd").value
+    }
+    localStorage.setItem("formData", JSON.stringify(formData));
+    console.log(localStorage.getItem("formData"));
+
+    e.preventDefault();
+    
+}
+
+///sign in
+
+
+localStorage.getItem("formData")
+
+
+
+function dos (sCorreo, sContrasenna){
+
+    const data = JSON.parse(localStorage.getItem("formData"))
+
+    if(data.email === sCorreo && data.pwd == sContrasenna){
+        console.log("bien")
+    } else{
+        console.log("nada")
+    }
+}
+
+
+
+function iniciarSesion(){
+    var sCorreo = "";
+    var sContrasenna = "";
+    var bAcceso;
+
+    sCorreo = document.querySelector("#txtCorreo").value
+    sContrasenna = document.querySelector("#txtContrasenna").value
+
+    bAcceso = dos(sCorreo, sContrasenna);
+    console.log(bAcceso);
+}
+
+
+document.querySelector("#btnIngresar").addEventListener("click", iniciarSesion);
+
