906a6fab23de3f049c1b6659d49bbf2206916934
shebiraaja <71530161+shebiraaja@users.noreply.github.com>
2021-01-20 17:51:50 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,28 +1,6 @@
-window.onload=function(){
-    setTimeout(logo,1000);
-    setTimeout(menu1, 1000);
-    setTimeout(menu2, 1000)
-}
+const menuIcon = document.querySelector(".mobile-icon");
+const navbar = document.querySelector(".main-nav");
 
-function logo(){
-    var h=document.querySelector("#logo h1");
-    h.style.opacity = "1"
-}
-
-function menu1(){
-    var l=document.querySelectorAll("#menu1 li")
-    var i;
-    for(i=0; i<l.length; i++){
-        l[i].style.opacity="1"
-        l[i].style.transform = "translate(0)"
-    }
-}
-
-function menu2(){
-    var l=document.querySelectorAll("#menu2 li")
-    var i;
-    for(i=0; i<l.length; i++){
-        l[i].style.opacity="1"
-        l[i].style.transform = "translate(0)"
-    }
-}
+menuIcon.addEventListener("click", () =>{
+    navbar.classList.toggle("change");
+})
