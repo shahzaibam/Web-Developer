5fd5e07e7c9427c95dffa2f3ea23aa6afc925c61
shebiraaja <71530161+shebiraaja@users.noreply.github.com>
2021-02-01 21:28:49 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,109 +1,83 @@
-let today = new Date();
-
-let thisMonth = today.getMonth();
-
-let thisYear = today.getFullYear();
-
-
-
-let chooseYear = document.getElementById("year");
-let chooseMonth = document.getElementById("month");
+function myTime(){
+    date = new Date();
+    hour = date.getHours()
+    min = date.getMinutes()
+    sec = date.getSeconds()
+    day = date.getDate()
+    month = date.getMonth()
+    year = date.getFullYear()
 
 
 
-let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+function addZero(x){
+    if(x<10){
+        x = "0" + x;
+    }
+
+    return x;
+}
 
 
-let monthAndYear  = document.getElementById("monthAndYear");
+day = addZero(day);
+min = addZero(min);
+sec = addZero(sec);
 
-displayCalendar(thisMonth, thisYear);
 
-//A function to display the next month from the month on the calender when clicked
+switch(month){
+    case 0:
+        month = "January"; break;
+    case 1:
+        month = "February"; break;
+    case 2:
+        month = "March"; break;
+    case 3:
+        month = "April"; break;
+    case 4:
+        month = "May"; break;
+    case 5:
+        month = "June"; break;
+    case 6:
+        month = "July"; break;
+    case 7:
+        month = "August"; break;
+    case 8:
+        month = "September"; break;
+    case 9:
+        month = "October"; break;
+    case 10:
+        month = "November"; break;
+    case 11:
+        month = "December"; break;
+}
 
-function nextMonth(){
-    if(thisMonth === 11){
-        thisYear = thisYear + 1;
+
+var mode;
+
+if(hour>11){
+    mode="PM"
+}else{
+    mode="AM"
+}
+
+document.getElementById("ampm").innerHTML = mode;
+
+
+//function for 12 hour format
+
+function timeformat(){
+    if(hour>12){
+        hour = hour - 12;
+    }else if(hour == 0){
+        hour = 12;
     }else{
-        thisYear = thisYear;
+        hour = hour;
     }
-    thisMonth = (thisMonth + 1) % 12; //we have only 12 months
-    displayCalendar(thisMonth,thisYear);
 }
 
-
-//A function to display the previous month from the month on the calender when clicked
-
-function previousMonth() {
-    if(thisMonth === 0){
-        thisYear = thisYear - 1;
-        thisMonth = 11; 
-    }else{
-        thisYear = thisYear;
-        thisMonth = thisMonth - 1;
-    }
-    displayCalendar(thisMonth, thisYear);
-}
-
-
-//A function that display the choosed month and year
-
-function goTo(){
-    thisYear = parseInt(chooseYear.value);
-    thisMonth = parseInt(chooseMonth.value);
-    displayCalendar(thisMonth, thisYear);
-}
-
-function displayCalendar(month, year){
-    let firstDay = (new Date(year, month)).getDay();
-    let daysInMonth = 32 - new Date(year, month, 32).getDate();
-
-    let tbl = document.getElementById("calendar-body");
-
-    //clearing all previous cell
-
-    tbl.innerHTML = "";
-
-    //filling data about month and in the page via DOM
-
-    monthAndYear.innerHTML  = months[month] + " " + year;
-    chooseYear.value = year;
-    chooseMonth.value = month;
-
-    //creating all cells
-    let date = 1;
-    for (let i = 0; i < 6; i++){
-        //create table row
-        let row = document.createElement("tr");
-
-        //creating individual cells, filling them up with data
-        for (let j = 0; j < 7; j++){
-            if(i === 0 && j < firstDay){
-                let cell = document.createElement("td");
-                let cellText = document.createTextNode("");
-                cell.appendChild(cellText);
-                row.appendChild(cell);
-            }else if(date>daysInMonth){
-                break;
-            }else{
-                let cell = document.createElement("td");
-                let cellText = document.createTextNode(date);
-                if(date === today.getDate() && year === today.getFullYear() && month === today.getMonth()){
-                    cell.classList.add("bg-secondary");
-                    cell.classList.add("text-white");
-                }//color todays date
-                cell.appendChild(cellText);
-
-                row.appendChild(cell);
-                date++;
-            }
-        }
-        tbl.appendChild(row)
-
-    }
-
+document.getElementById("time").innerHTML = hour + ":" + min + ":" + sec;
+document.getElementById("date").innerHTML = date + ":" + month + ":" + year;
 
 
 
 
 }
-
