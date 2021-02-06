c2b7ebdb0ddec96e39fa6ae62b7db7e400d551ce
shebiraaja <71530161+shebiraaja@users.noreply.github.com>
2021-02-06 18:05:04 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,28 +1,29 @@
 function startTime(){
     var today = new Date();
-    var date = today.getDate()
+    var hour = today.getHours()
+    var min = today.getMinutes()
+    var sec = today.getSeconds()
+    var day = today.getDate()
     var month = today.getMonth()
     var year = today.getFullYear()
-    var hours = today.getHours()
-    var min = today.getMinutes()
-    var sec = today.getSeconds() 
 
     function addZero(x){
         if(x<10){
-            x = "0" + x
+            x = "0" + x;
         }
+
         return x;
     }
 
-    date = addZero(date)
-    min = addZero(min)
-    sec = addZero(sec)
+    day = addZero(day);
+    min = addZero(min);
+    sec = addZero(sec);
 
     switch(month){
         case 0:
             month = "January"; break;
         case 1:
-            month = "February"; break;
+            month = "February"; break;ç
         case 2:
             month = "March"; break;
         case 3:
@@ -42,33 +43,36 @@ function startTime(){
         case 10:
             month = "November"; break;
         case 11:
-            month = "December"; break;    
+            month = "December"; break;
     }
 
     var mode;
-    if(hours>11){
+
+    if(hour>11){
         mode = "PM"
     }else{
         mode = "AM"
     }
 
-    document.getElementById("ampm").innerHTML = mode;
+    document.getElementsByClassName("ampm").innerHTML = mode;
 
-
-    function timeFormat(){
-        if(hours>12){
-            hours = hours - 12;
-        }else if(hours == 0){
-            hours = 12
-        }else{
-            hours = hours
-        }
+    //12 hour format
+    
+function timeFormat(){
+    if(hour>12){
+        hour = hour - 12;
+    }else if(hour == 0){
+        hour = 12;
+    }else{
+        hour = hour;
     }
+}
 
-    document.getElementById("time").innerHTML = hours + ":" + min + ":" + sec;
-    document.getElementById("date").innerHTML = date + ":" + month + ":" + year;
+document.getElementById("time").innerHTML = hour + ":" + min + ":" + sec;
+document.getElementById("date").innerHTML = day + month + year;
 
 }
+
 
 function daysOfWeek(){
     var date = new Date();
@@ -76,54 +80,49 @@ function daysOfWeek(){
 
     switch(days){
         case 0:
-            days = "Sunday";
+            days = "Sunday"
             document.getElementById("sun").style.background = "tan";
             document.getElementById("sun").style.borderRadius = "2px";
             document.getElementById("sun").style.color = "red";
             document.getElementById("sun").style.boxShadow = "1px 1px 2px 2px white";
             break;
-        case 1:  
-            days = "Monday";
+        case 1:
+            days = "Monday"
             document.getElementById("mon").style.background = "tan";
             document.getElementById("mon").style.borderRadius = "2px";
             document.getElementById("mon").style.color = "red";
             document.getElementById("mon").style.boxShadow = "1px 1px 2px 2px white";
             break;
-
-        case 2:
-            days = "Tuesday";
+        case 0:
+            days = "Tuesday"
             document.getElementById("tue").style.background = "tan";
             document.getElementById("tue").style.borderRadius = "2px";
             document.getElementById("tue").style.color = "red";
             document.getElementById("tue").style.boxShadow = "1px 1px 2px 2px white";
             break;
-
-        case 3:  
-            days = "Wednesday";
+        case 0:
+            days = "Wednesday"
             document.getElementById("wed").style.background = "tan";
             document.getElementById("wed").style.borderRadius = "2px";
             document.getElementById("wed").style.color = "red";
-            document.getElementById("wed").style.boxShadow = "1px 1px 2px 2px white"; 
+            document.getElementById("wed").style.boxShadow = "1px 1px 2px 2px white";
             break;
-
-        case 4:
-            days = "Thursday";
+        case 0:
+            days = "Thursday"
             document.getElementById("thu").style.background = "tan";
             document.getElementById("thu").style.borderRadius = "2px";
             document.getElementById("thu").style.color = "red";
             document.getElementById("thu").style.boxShadow = "1px 1px 2px 2px white";
             break;
-
-        case 5:  
-            days = "Friday";
+        case 0:
+            days = "Friday"
             document.getElementById("fri").style.background = "tan";
             document.getElementById("fri").style.borderRadius = "2px";
             document.getElementById("fri").style.color = "red";
             document.getElementById("fri").style.boxShadow = "1px 1px 2px 2px white";
             break;
-
-        case 6:
-            days = "Saturday";
+        case 0:
+            days = "Saturday"
             document.getElementById("sat").style.background = "tan";
             document.getElementById("sat").style.borderRadius = "2px";
             document.getElementById("sat").style.color = "red";
@@ -133,5 +132,3 @@ function daysOfWeek(){
 }
 
 daysOfWeek()
-
-
