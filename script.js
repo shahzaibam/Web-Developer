6a5c6ff684bd9f55a6974e4210c04adac8cc7be8
shebiraaja <71530161+shebiraaja@users.noreply.github.com>
2021-03-03 22:22:39 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,50 +1,50 @@
+dconst { parse } = require("path");
+
 let vacationCalc = document.getElementById("vacationCalc")
 
 vacationCalc.addEventListener("submit", calcExpenses);
 
-
 function getValues(){
-    let destiny = document.getElementById("destiny").value;
-        budget = document.getElementById("budget").value;
-        acomodation = document.getElementById("acomodation").value;
-        transport = document.getElementById("transport").value;
-        food = document.getElementById("food").value;
+    let destiny  = document.getElementById("destiny").value;
+        budget  = document.getElementById("budget").value;
+        acomodation  = document.getElementById("acomodation").value;
+        transport  = document.getElementById("transport").value;
+        food  = document.getElementById("food").value;
     return {destiny, budget, acomodation, transport, food}
 }
 
 function calcExpenses(e){
     e.preventDefault()
 
-    const {destiny, budget, acomodation, transport, food} = getValues();
+    const{destiny,budget,acomodation,transport,food} = getValues()
 
     let expenses = parseInt(acomodation) + parseInt(transport) + parseInt(food)
     let balance = budget - expenses
 
     UI(destiny, budget, balance)
-    
 }
 
 function UI(){
     let result = document.getElementById("result");
-    let dataPrint = document.createElement("div");
+    let dataPrint = document.getElementById("div");
 
     dataPrint.innerHTML = `
-        <div class="container-data row">
-            <div class"col s4">
+        <div class="container-data row>
+            <div class="col s4">
                 <h6>${destiny}</h6>
             </div>
 
-            <div class"col s4">
+            <div class="col s4">
                 <h6>${budget}</h6>
             </div>
 
-            <div class"col s4">
+            <div class="col s4">
                 <h6>${balance}</h6>
             </div>
         </div>
     `
-    result.appendChild(dataPrint)
 
+    result.appendChild(dataPrint)
     reset();
 }
 
