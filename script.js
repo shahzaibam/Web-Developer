ee911f6e3cfa43c47f25990ede66695572d82999
shebiraaja <71530161+shebiraaja@users.noreply.github.com>
2021-03-07 16:02:45 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,53 +1,21 @@
-dconst { parse } = require("path");
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
 
-let vacationCalc = document.getElementById("vacationCalc")
+todoButton.addEventListener("click", addTodo);
 
-vacationCalc.addEventListener("submit", calcExpenses);
- 
-function getValues(){
-    let destiny  = document.getElementById("destiny").value;
-        budget  = document.getElementById("budget").value;
-        acomodation  = document.getElementById("acomodation").value;
-        transport  = document.getElementById("transport").value;
-        food  = document.getElementById("food").value;
-    return {destiny, budget, acomodation, transport, food}
+function addTodo(event){
+    event.preventDefault();
+
+    //create todo Div
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+
+    //create todo li
+    const newTodo = document.createElement("li");
+    newTodo.innerText = "hey";
+    newTodo.classList.add("todo-item");
+    todoDiv.appendChild(newTodo)
 }
 
-function calcExpenses(e){
-    e.preventDefault()
 
-    const{destiny,budget,acomodation,transport,food} = getValues()
-
-    let expenses = parseInt(acomodation) + parseInt(transport) + parseInt(food)
-    let balance = budget - expenses
-
-    UI(destiny, budget, balance)
-}
-
-function UI(){
-    let result = document.getElementById("result");
-    let dataPrint = document.getElementById("div");
-
-    dataPrint.innerHTML = `
-        <div class="container-data row>
-            <div class="col s4">
-                <h6>${destiny}</h6>
-            </div>
-
-            <div class="col s4">
-                <h6>${budget}</h6>
-            </div>
-
-            <div class="col s4">
-                <h6>${balance}</h6>
-            </div>
-        </div>
-    `
-
-    result.appendChild(dataPrint)
-    reset();
-}
-
-function reset(){
-    document.getElementById("vacationCalc").reset()
-}
