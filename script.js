502e0063872f0a3d140062598021f0bfc4b90027
shebiraaja <71530161+shebiraaja@users.noreply.github.com>
2021-03-19 21:24:16 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
-
+   
 todoButton.addEventListener("click", addTodo);
 
 function addTodo(event){
