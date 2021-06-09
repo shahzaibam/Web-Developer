df98e6b2e8576c4eeb0f1d6d23a6003488346944
shebiraaja <71530161+shebiraaja@users.noreply.github.com>
2021-06-09 12:19:05 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,106 +1,44 @@
-const carrousel = document.querySelector(".carrousel") 
-const carrouselImg = document.querySelectorAll(".carrousel img")
-const nav = document.querySelector(".nav")
+const next = document.querySelector(".arrow-next")
+const prev = document.querySelector(".arrow-prev")
 
-const prevBtn = document.querySelector("#prev")
-const nextBtn = document.querySelector("#next")
+let slide_index = 0;
 
-let counter = 1;
-let size = carrouselImg[0].clientWidth;
+function showSlide(index){
+    let slide = document.querySelectorAll(".slide")
+    let dots = document.querySelectorAll(".dot-nav")
 
-
-carrousel.style.transform = "translateX("+(-size * counter) + "px)";
-
-document.addEventListener("keyup", (e)=>{
-    if(e.keyCode==39){
-        if(counter >= carrouselImg.length - 1 )return;
-        carrousel.style.transition = "transform 0.4s ease-in-out"
-        counter++;
-        carrousel.style.transform = "translateX("+(-size * counter) + "px)";
-        console.log(size)
-    }
-})
-
-nextBtn.addEventListener("click", ()=>{
-    if(counter >= carrouselImg.length - 1 )return;
-    carrousel.style.transition = "transform 0.4s ease-in-out"
-    counter++;
-    carrousel.style.transform = "translateX("+(-size * counter) + "px)";
-
-})
-
-
-
-prevBtn.addEventListener("click", ()=>{
-    if(counter<=0)return;
-    carrousel.style.transition = "transform 0.4s ease-in-out"
-    counter--;
-    carrousel.style.transform = "translateX("+(-size * counter) + "px)";
-})
-
-document.addEventListener("keyup", (e)=>{
-    if(e.keyCode==37){
-
-        if(counter <=0 )return;
-        carrousel.style.transition = "transform 0.4s ease-in-out"
-        counter--;
-        carrousel.style.transform = "translateX("+(-size * counter) + "px)";
-    }
-})
-
-carrousel.addEventListener("transitionend", ()=>{
-    console.log(carrouselImg[counter])
-    if(carrouselImg[counter].id === "abcd"){
-        carrousel.style.transition = "none"
-        counter = carrouselImg.length - 2;
-        carrousel.style.transform = "translateX("+(-size * counter) + "px)";
+    if(index >= slide.length){
+        slide_index = 0;
     }
 
-    if(carrouselImg[counter].id === "efgh"){
-        carrousel.style.transition = "none"
-        counter = carrouselImg.length - counter;
-        carrousel.style.transform = "translateX("+(-size * counter) + "px)";
+    if(index < 0 ){
+        slide_index = slide.length - 1;
     }
 
-})
+    for(let i = 0; i<slide.length; i++){
+        slide[i].style.display = "none"
+        dots[i].classList.remove("active-dot")
+    }
+    slide[slide_index].style.display = "block"
+    dots[slide_index].classList.add("active-dot")
 
-
-nav.addEventListener("click", function(e){
-
-    if(e.target === nav) return;
-
-    const targetdot = e.target;
-
-    const currentDot = nav.querySelector(".active")
-
-    const currentSlide = carousel.querySelector(".active")
-
-    let targetDotIndex = findIndex(targetdot, dots)
-
-    const targetSlide = slides[targetDotIndex]
-
-    moveToSlide(carousel, currentSlide, targetSlide)
-    toggleActive(currentDot, targetdot)
-    hideButton(targetSlide, slides)
-})
-
-
-function dotToSlide(targetSlide, slides, nav, dots){
-    let slideIndex = findIndex(targetSlide, slides);
-    const currentDot = nav.querySelector(".active");
-    const targetdot = dots[slideIndex]
-    toggleActive(currentDot, targetdot)
 }
 
-function toggleActive(current, target){
-    current.classList.remove("active");
-    target.classList.add("active")
-}
+showSlide(slide_index)
 
-function findIndex(item, items){
-    for(let index = 0; index < items.length; index++){
-        if(item===items[index]){
-            return index;
-        }
-    }
-}
+prev.addEventListener("click", function(){
+    showSlide(slide_index = slide_index - 1)
+})
+
+next.addEventListener("click", function(){
+    showSlide(slide_index = slide_index + 1)
+})
+
+document.querySelectorAll(".dot-nav").forEach((element)=>{
+    element.addEventListener("click", function(){
+        var dots = Array.prototype.slice.call(element.parentElement
+            .children),
+        dot_index = dots.indexOf(element)
+        showSlide(slide_index = dot_index ) 
+    })
+})
