6210118f84c17d93ce6f6a98c9397d0b5b4ab2a1
shebiraaja <71530161+shebiraaja@users.noreply.github.com>
2021-06-09 12:20:29 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,40 +1,47 @@
-let slider_index = 0;
+ const next = document.querySelector(".arrow-next")
+ const prev = document.querySelector(".arrow-prev")
+ let slider_index = 0;
 
-function showSlide(index){
-    let slide = document.querySelectorAll(".slide")
-    let dots = document.querySelectorAll(".dot-nav")
 
-    if(index >= slide.length){
-        slider_index = 0;
-    }
+ function showSlide(index){
+     let slide = document.querySelectorAll(".slide")
+     let dots = document.querySelectorAll(".nav-dot")
 
-    if(index<0){
-        slider_index = slide.length - 1
-    }
 
-    for(let i=0; i<slide.length; i++){
-        slide[i].style.display = "none"
-        dots[i].classList.remove("active-dot")
-    }
 
-    slide[slider_index].style.display = "block"
-    dots[slider_index].classList.add("active-dot")
-}
+     if(index>=slide.length){
+         slider_index = 0;
+     }
 
-showSlide(slider_index)
+     if(index<0){
+         slider_index = slide.length -1 
+     }
 
-document.querySelector(".arrow-prev").addEventListener("click", function(){
-    showSlide(slider_index = slider_index - 1)
-})
+     for(let i = 0; i<slide.length; i++){
+         slide[i].style.display = "none"
+         dots[i].classList.remove("active-dot")
+     }
 
-document.querySelector(".arrow-next").addEventListener("click", function(){
-    showSlide(slider_index = slider_index + 1)
-})
+     slide[slider_index].style.display = "block"
+     dots[slider_index].classList.add("active-dot")
 
-document.querySelectorAll(".dot-nav").forEach((element)=>{
-    element.addEventListener("click", function(){
-        var dots = Array.prototype.slice.call(element.parentElement.children),
-        dot_index = dots.indexOf(element)
-        showSlide(slider_index  =  dot_index)
-    })
-})
+ }
+
+ showSlide(slider_index)
+
+ prev.addEventListener("click", function(){
+     showSlide(slider_index = slider_index - 1)
+ })
+
+ next.addEventListener("click", function(){
+     showSlide(slider_index = slider_index + 1)
+ })
+
+ document.querySelectorAll(".nav-dot").forEach((element)=>{
+     element.addEventListener("click", function(){
+     var dots = Array.prototype.slice.call(element.parentElement.children),
+     dot_index = dots.indexOf(element)
+     showSlide(slider_index = dot_index)
+     })
+ })
+ 
