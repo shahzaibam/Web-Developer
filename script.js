cf6042ad3e9b10cda76054345e05f4a13cffa7d4
shebiraaja <71530161+shebiraaja@users.noreply.github.com>
2021-06-17 13:23:21 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,44 +1,37 @@
-// api key : 82005d27a116c2880c8f0fcb866998a0
+const notification = document.querySelector(".notification")
+const weather_container = document.querySelector(".weather-container")
+const weather_icon = document.querySelector(".weather-icon")
+const temperature_value = document.querySelector(".temperature-value")
+let temp_description = document.querySelector(".temperature-description")
+const location_ = document.querySelector(".location")
 
-const iconElement = document.querySelector(".weather-icon")
-const tempElement = document.querySelector(".temperature-value p")
-const descElement = document.querySelector(".temperature-description p")
-const locationElement = document.querySelector(".location p")
-const notificationElement = document.querySelector(".notification")
-
-//App Data
 const weather = {}
 
 weather.temperature = {
     unit : "celsius"
 }
 
-//App const and vars
 const KELVIN = 273;
 
-//API key
-
 const key = "ef7877b8c666feed22d2d02c567aabbc"
-
-//check if browser supports geolocation
 
 if(navigator.geolocation){
     navigator.geolocation.getCurrentPosition(setPosition, showError)
 }else{
-    notificationElement.style.display = "block"
-    notificationElement.innerHTML = "<p>Browser doesn't support geolocation</p>"
+    navigator.style.display = "block"
+    navigator.innerHTML = `<p>Browser doesn't support Geolocation</p>`
 }
 
 function setPosition(position){
-    let latitude = position.coords.latitude;
-    let longitude = position.coords.longitude;
+    let latitude = position.coords.latitude
+    let longitude = position.coords.longitude
 
-    getWeather(latitude,longitude)
+    getWeather(latitude, longitude)
 }
 
 function showError(error){
-    notificationElement.style.display = "block"
-    notificationElement.innerHTML = `<p>${error.message}</p>`
+    navigator.style.display = "block"
+    navigator.innerHTML = `<p>${error.message}</p>`
 }
 
 function getWeather(latitude, longitude){
@@ -51,39 +44,44 @@ function getWeather(latitude, longitude){
         })
         .then(function(data){
             weather.temperature.value = Math.floor(data.main.temp - KELVIN)
-            weather.main = data.weather[0].main;
-            weather.iconId = data.weather[0].icon;
-            weather.city = data.name;
+            weather.main = data.weather[0].main
+            weather.iconId = data.weather[0].icon
+            weather.city = data.name
             weather.country = data.sys.country
         })
         .then(function(){
-            displayWeather();
-        });
+            displayWeather()
+        })
 }
 
 function displayWeather(){
-    iconElement.innerHTML = `<img src="icons/${weather.iconId}.png"/>`
-    tempElement.innerHTML = `${weather.temperature.value}º<span>C</span>`
-    descElement.innerHTML = `<p>${weather.main}</p>`
-    locationElement.innerHTML = `${weather.city}, ${weather.country}`
+    weather_icon.innerHTML = `<img src="icons/${weather.iconId}.png">`
+    temperature_value.innerHTML = `<p>${weather.temperature.value}º <span>C</span><p>`
+    temp_description = `<p>${weather.main}</p>`
+    location_.innerHTML = `<p>${weather.city}, ${weather.country}</p>`
 }
+
 
 function celsiusToFahrenheit(temperature){
     return (temperature * 9/5) + 32;
 }
 
-tempElement.addEventListener("click", function(){
+temperature_value.addEventListener("click", function(){
     if(weather.temperature.value === undefined) return;
 
     if(weather.temperature.unit === "celsius"){
         let fahrenheit = celsiusToFahrenheit(weather.temperature.value);
         fahrenheit = Math.floor(fahrenheit)
 
-        tempElement.innerHTML = `${fahrenheit}º<span>F</span>`
+        temperature_value.innerHTML = `<p>${fahrenheit}º<span>F</span></p>`
         weather.temperature.unit = `${weather.temperature.value}`
     }else{
-        tempElement.innerHTML =`${weather.temperature.value}º<span>C</span>`
+        temperature_value.innerHTML =`<p>${weather.temperature.value}º<span>C</span></p>`
         weather.temperature.unit = "celsius"
     }
 
 })
+
+
+
+
