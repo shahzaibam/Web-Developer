bf1b376f80b5d29e2f4772c6a86670c8160f16fe
shebiraaja <71530161+shebiraaja@users.noreply.github.com>
2021-06-19 13:21:42 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,86 +1,187 @@
-const notification = document.querySelector(".notification")
-const weather_container = document.querySelector(".weather-container")
-const weather_icon = document.querySelector(".weather-icon")
-const temperature_value = document.querySelector(".temperature-value")
-const temperature_description = document.querySelector(".temperature-description")
-const location_ = document.querySelector(".location")
+const next = document.querySelector(".arrow-next")
+const prev = document.querySelector(".arrow-prev")
+let slider_index = 0;
 
 
-const weather = {}
-
-weather.temperature = {
-    unit : "celsius"
-}
-
-const KELVIN = 273;
-
-const key = "ef7877b8c666feed22d2d02c567aabbc"
-
-if(navigator.geolocation){
-    navigator.geolocation.getCurrentPosition(setPosition, showError)
-}else{
-    notification.style.display = "block"
-    notification.innerHTML = `<p>Browser doesn't support geolocation</p>`
-}
-
-function setPosition(position){
-    let latitude = position.coords.latitude
-    let longitude = position.coords.longitude
-
-    getWeather(latitude, longitude)
-}
-
-function showError(error){
-    notification.style.display = "block"
-    notification.innerHTML = `<p>${error.message}</p>`
-}
+function showSlide(index){
+    let slide = document.querySelectorAll(".slide")
+    let dots = document.querySelectorAll(".nav-dot")
 
 
-function getWeather(latitude, longitude){
-    let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`
 
-    fetch(api)
-        .then(function(response){
-            let data = response.json()
-            return data
-        })
-        .then(function(data){
-            weather.temperature.value = Math.floor(data.main.temp - KELVIN)
-            weather.main = data.weather[0].main
-            weather.iconId = data.weather[0].icon
-            weather.city = data.name
-            weather.country = data.sys.country
-
-        })
-        .then(function(){
-            displayWeather()
-        })
-}
-
-function displayWeather(){
-    weather_icon.innerHTML = `<img src="/icons/${weather.iconId}.png">`
-    temperature_value.innerHTML = `<p>${weather.temperature.value}ºC</p>`
-    temperature_description.innerHTML = `<p>${weather.main}</p>`
-    location_.innerHTML = `<p>${weather.city}, ${weather.country}</p>`
-}
-
-function celsiusToFarehnheit(temperature){
-    return (temperature * 9/5)+ 32
-}
-
-temperature_value.addEventListener("click", function(){
-    if(weather.temperature.value === undefined) return;
-
-    if(weather.temperature.unit === "celsius"){
-        let farehnheit = celsiusToFarehnheit(weather.temperature.value)
-        farehnheit = Math.floor(farehnheit)
-
-        temperature_value.innerHTML = `<p>${farehnheit}º<span>F</span></p>`
-        weather.temperature.unit = `${weather.temperature.value}`
-    }else{
-        temperature_value.innerHTML =`<p>${weather.temperature.value}º<span>C</span></p>`
-        weather.temperature.unit = "celsius"
+    if(index>=slide.length){
+        slider_index = 0;
     }
+
+    if(index<0){
+        slider_index = slide.length -1 
+    }
+
+    for(let i = 0; i<slide.length; i++){
+        slide[i].style.display = "none"
+        dots[i].classList.remove("active-dot")
+    }
+
+    slide[slider_index].style.display = "block"
+    dots[slider_index].classList.add("active-dot")
+
+}
+
+showSlide(slider_index)
+
+prev.addEventListener("click", function(){
+    showSlide(slider_index = slider_index - 1)
+})
+
+next.addEventListener("click", function(){
+    showSlide(slider_index = slider_index + 1)
+})
+
+document.querySelectorAll(".nav-dot").forEach((element)=>{
+    element.addEventListener("click", function(){
+    var dots = Array.prototype.slice.call(element.parentElement.children),
+    dot_index = dots.indexOf(element)
+    showSlide(slider_index = dot_index)
+    })
+})
+
+
+let calculator_buttons = [
+    {
+        name : "delete",
+        symbol : "⌫",
+        formula : false,
+        type : "key"
+    },{
+        name : "clear",
+        symbol : "C",
+        formula : false,
+        type : "key"
+    },{
+        name : "percent",
+        symbol : "%",
+        formula : "/100",
+        type : "number"
+    },{
+        name : "division",
+        symbol : "÷",
+        formula : "/",
+        type : "operator"
+    },{
+        name : "7",
+        symbol : 7,
+        formula : 7,
+        type : "number"
+    },{
+        name : "8",
+        symbol : 8,
+        formula : 8,
+        type : "number"
+    },{
+        name : "9",
+        symbol : 9,
+        formula : 9,
+        type : "number"
+    },{
+        name : "multiplication",
+        symbol : "×",
+        formula : "*",
+        type : "operator"
+    },{
+        name : "4",
+        symbol : 4,
+        formula : 4,
+        type : "number"
+    },{
+        name : "5",
+        symbol : 5,
+        formula : 5,
+        type : "number"
+    },{
+        name : "6",
+        symbol : 6,
+        formula : 6,
+        type : "number"
+    },{
+        name : "addition",
+        symbol : "+",
+        formula : "+",
+        type : "operator"
+    },,{
+        name : "1",
+        symbol : 1,
+        formula : 1,
+        type : "number"
+    },{
+        name : "2",
+        symbol : 2,
+        formula : 2,
+        type : "number"
+    },{
+        name : "3",
+        symbol : 3,
+        formula : 3,
+        type : "number"
+    },{
+        name : "subtraction",
+        symbol : "–",
+        formula : "-",
+        type : "operator"
+    },{
+        name : "0",
+        symbol : 0,
+        formula : 0,
+        type : "number"
+    },{
+        name : "comma",
+        symbol : ".",
+        formula : ".",
+        type : "number"
+    },{
+        name : "calculate",
+        symbol : "=",
+        formula : "=",
+        type : "calculate"
+    }
+];
+
+
+const input_element = document.querySelector(".input")
+const output_operation_element = document.querySelector(".operation .value")
+const output_result_element = document.querySelector(".result .value")
+
+
+function showButton(){
+    const btns_per_row = 4;
+    let added_btns = 0;
+
+    calculator_buttons.forEach(button=>{
+        if(added_btns % btns_per_row == 0){
+            input_element.innerHTML += `<div class="row"></div>`
+        }
+    
+        const row = document.querySelector(".row:last-child")
+    
+        row.innerHTML += `<button id="${button.name}">
+                            ${button.symbol}  
+                            </button>                  
+        `
+        added_btns++;
+    })
+}
+
+
+showButton()
+
+
+input_element.addEventListener("click", event =>{
+    let targetedBtn = event.target;
+
+    calculator_buttons.forEach(button=>{
+        if(targetedBtn == button.id){
+            return calculate(button)
+        }
+    })
 })
 
 
