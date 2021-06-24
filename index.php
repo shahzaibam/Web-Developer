c1418593dc9a0178649d5906f521d6431867d9d7
shebiraaja <71530161+shebiraaja@users.noreply.github.com>
2021-06-24 20:23:14 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -22,26 +22,26 @@ require_once("config.php")
 <body>
 
     <div class="container">
-        <h1 id="heading-taxiProject">Taxi Project</h1>
+        <h1 id="heading-taxiProject">Taxi <span style="color: #FFFF00;">Project</span></h1>
         <table>
             <thead id="table-heading" style="background-color: white; color: black;">
-            <th id="NUM">NUM</th>    
-            <th id="FECHA">FECHA</th>
-                <th id="KILOMETROS">KILOMETROS</th>
-                <th id="ACEITE">ACEITE</th>
-                <th id="FILTRO_ACEITE">FILTRO ACEITE</th>
-                <th id="FILTRO_AIRE">FILTRO AIRE</th>
-                <th id="FILTRO_POLEN">FILTRO POLEN</th>
-                <th id="FILTRO_GASOIL">FILTRO GASOIL</th>
-                <th id="DELANTERAS">PASTILLAS<BR> DELANTERAS</th>
-                <th id="TRASERAS">PASTILLAS<BR>  TRASERAS</th>
-                <th id="DISCOS_DELANTEROS">DISCOS <BR>  DELANTEROS</th>
-                <th id="DISCOS_TRASEROS">DISCOS <BR>  TRASEROS</th>
-                <th id="RUEDAS_DELANTERAS">RUEDAS <BR>  DELANTERAS</th>
-                <th id="RUEDAS_TRASERAS">RUEDAS <BR>  TRASERAS</th>
-                <th id="EMBRAGUE">EMBRAGUE</th>
-                <th id="OTROS">OTROS</th>
-                <th id="observation">OBSERVACIONS</th>
+            <th id="NUM" style="background-color: #355c7d; color: #fff;">NUM</th>    
+            <th id="FECHA" style="background-color: #355c7d; color: #fff;">FECHA</th>
+                <th id="KILOMETROS"style="background-color: #355c7d; color: #fff;">KILOMETROS</th>
+                <th id="ACEITE" style="background-color: #355c7d; color: #fff;">ACEITE</th>
+                <th id="FILTRO_ACEITE" style="background-color: #355c7d; color: #fff;">FILTRO ACEITE</th>
+                <th id="FILTRO_AIRE" style="background-color: #355c7d; color: #fff;">FILTRO AIRE</th>
+                <th id="FILTRO_POLEN" style="background-color: #355c7d; color: #fff;">FILTRO POLEN</th>
+                <th id="FILTRO_GASOIL" style="background-color: #355c7d; color: #fff;">FILTRO GASOIL</th>
+                <th id="DELANTERAS" style="background-color: #355c7d; color: #fff;">PASTILLAS<BR> DELANTERAS</th>
+                <th id="TRASERAS" style="background-color: #355c7d; color: #fff;">PASTILLAS<BR>  TRASERAS</th>
+                <th id="DISCOS_DELANTEROS" style="background-color: #355c7d; color: #fff;">DISCOS <BR>  DELANTEROS</th>
+                <th id="DISCOS_TRASEROS" style="background-color: #355c7d; color: #fff;">DISCOS <BR>  TRASEROS</th>
+                <th id="RUEDAS_DELANTERAS" style="background-color: #355c7d; color: #fff;">RUEDAS <BR>  DELANTERAS</th>
+                <th id="RUEDAS_TRASERAS" style="background-color: #355c7d; color: #fff;">RUEDAS <BR>  TRASERAS</th>
+                <th id="EMBRAGUE" style="background-color: #355c7d; color: #fff;">EMBRAGUE</th>
+                <th id="OTROS" style="background-color: #355c7d; color: #fff;">OTROS</th>
+                <th id="observation" style="background-color: #355c7d; color: #fff;">OBSERVACIONS</th>
             </thead>
             <tbody>
             <?php
@@ -74,7 +74,7 @@ require_once("config.php")
             
         </table>
 
-        <button id="add">Add</button>
+        <a href="/project/input.php"><button id="add">Add</button></a>
        
     </div>
 
