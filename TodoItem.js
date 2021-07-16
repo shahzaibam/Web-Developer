53668d36689c44d21c31160d9ae9010e615effc5
shebiraaja <71530161+shebiraaja@users.noreply.github.com>
2021-07-16 19:28:13 +0200

    Add files via upload

---
+++ TodoItem.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const TodoItem = ({todo, onDelete}) => {
+    return (
+        <div>
+            <h1>{todo.title}</h1>
+            <p>{todo.desc}</p>
+            <button className="btn-btn-danger" onClick={()=>{onDelete(todo)}}>Delete</button>
+
+        </div>
+    )
+}
