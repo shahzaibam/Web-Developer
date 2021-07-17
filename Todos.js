12cf2b5892a6b47b1a2ee0e889f4537ce224e9b6
shebiraaja <71530161+shebiraaja@users.noreply.github.com>
2021-07-17 19:54:27 +0200

    Add files via upload

--- Todos.js
+++ Todos.js
@@ -5,6 +5,7 @@ export const Todos = (props) => {
     return (
         <div className="container">
             <h3 className="text-center my-10">Todos List</h3>
+            {props.todos.length === 0 ? "no todos left" :""}
             {props.todos.map((todo)=>{
                 return  <TodoItem key={todo.sno} todo={todo} onDelete={props.onDelete} />
             })}
