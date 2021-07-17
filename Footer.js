56e29d0644b3576c5fc6d8d774f6ce4b377e0d49
shebiraaja <71530161+shebiraaja@users.noreply.github.com>
2021-07-17 19:53:57 +0200

    Add files via upload

--- Footer.js
+++ Footer.js
@@ -1,9 +1,16 @@
 import React from 'react'
 
 export const Footer = () => {
+    let footerStyle = {
+        position : "absolute",
+        top : "114vh",
+        width : "100%"
+    }
     return (
-        <div>
-            footer works!
-        </div>
+        <footer className="bg-dark text-light py-3" style={footerStyle}>
+            <p className="text-center">
+                Copyright &copy; Shebi'sTodoList.com
+            </p>
+        </footer>
     )
 }
