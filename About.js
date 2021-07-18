0d8fa16bc85acf4b7a7e3a1826c466a5923b31a8
shebiraaja <71530161+shebiraaja@users.noreply.github.com>
2021-07-18 16:54:41 +0200

    Add files via upload

---
+++ About.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, architecto modi ex explicabo dolorem debitis a ipsum enim harum saepe aut, veritatis unde fuga maiores nisi, ducimus sequi? Quaerat aspernatur obcaecati asperiores ab dolorem.</p>
+        </div>
+    )
+}
