a1284ff8c346c2f50dc39a8385ee32f6d6d27ebb
shebiraaja <71530161+shebiraaja@users.noreply.github.com>
2021-07-23 19:18:09 +0200

    Add files via upload

---
+++ players.js
@@ -0,0 +1,20 @@
+import cristiano from './cristiano.png';
+import messi from './messi.png';
+import neymar from './neymar.png';
+
+export const players = {
+    cristiano: {
+        image : {cristiano},
+        facts : ["Es un jugador dinámico y veloz que se aprovecha de los espacios ofensivos", "Es diestro, aunque maneja por igual la zurda, esto le permite jugar por las dos bandas", "Desborda por los costados constantemente debido a su facilidad para salir por fuera o por dentro y a sus variados regates."]
+    },
+
+    messi: {
+        image : {messi},
+        facts : ["Está lleno de energía, siempre en movimiento, inquieto y bastante hiperactivo", "La vida está en constante movimiento para él y la devora con fuerza", "Tiene una gran habilidad para poner los pensamientos en acción"]
+    },
+
+    neymar: {
+        image : {neymar},
+        facts : ["Neymar es muy rápido y tiene un gran control del balón", "Neymar es capaz de inventar en un metro cuadrado y rodeado de piernas rivales", "El brasileño sabe interpretar a sus compañeros y moverse hacia los huecos libres para recibir el balón con el mayor peligro posible"]
+    }
+};
