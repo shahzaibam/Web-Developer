ce105c8ae7db38822a96306fad1f24707bdfdb9f
shebiraaja <71530161+shebiraaja@users.noreply.github.com>
2021-07-27 19:57:45 +0200

    Add files via upload

--- App.js
+++ App.js
@@ -1,152 +1,67 @@
 import logo from './logo.svg';
-import cancha from './cancha.jpg';
-import cristiano from './cristiano.png';
-import messi from './messi.png';
-import neymar from './neymar.png';
 import './App.css';
-import {players} from "./players"
-import React from "react"
+import React from 'react'
 import ReactDOM from 'react-dom';
 
-const title = ""
+class Contact extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      password: '123456789',
+      authorized: false
+    }
+    this.authorize = this.authorize.bind(this) 
+  }
 
-const showBackground = true;
+  authorize(e) {
+    const password = e.target.querySelector('input[type = "pasword"]').value
 
-const style_back_img = {
-  marginLeft : "25%",
+    const auth = password == this.state.password
+
+    this.setState({
+      authorized: auth
+    })
+  }
+
+  render() {
+    let login = (
+      <form action="#" onSubmit={this.authorize}>
+        <input type="password" placeholder="Password" />
+        <input type="submit" />
+      </form>
+    )
+
+    let contactInfo = (
+      <ul>
+        <li>
+          Home
+        </li>
+
+        <li>
+        Home
+        </li>
+
+        <li>
+        Home
+        </li>
+      </ul>
+      )
+
+
+      return (
+        <div id="authoriziation">
+          <h1>{this.state.password ? 'contact' : 'Ennter password'}</h1>
+        </div>
+      )
+  }
 }
 
-const backgroundImg = <img src={cancha} style={style_back_img }/>
+ReactDOM.render(<Contact />, document.getElementById("app"))
 
-const style_h1 = {
-  marginLeft : "40%",
-}
-
-const images = []
-
-// for(const player in players){
-//   images.push(
-//     <img
-//       key = {player}
-//       className = "player"
-//       alt = {player}
-//       src = {players[player].image}
-//       aria-label = {player}
-//       role = "button"
-//       onClick = {displayFact}
-//     />
-//   )
-// }
-
-const styleCris = {
-  position : "absolute",
-  top : "180px",
-  left : "350px",
-  width : "200px"
-}
-
-images.push(
-    <img
-      key = "cristiano"
-      className = "cristiano"
-      alt = "cristiano"
-      src = {cristiano}
-      aria-label = "cristiano"
-      role = "button"
-      onClick = {displayFact}
-      style = {styleCris}
-    />
-)
-
-const styleMessi = {
-  position : "absolute",
-  top : "180px",
-  right : "570px",
-  width : "200px"
-}
-
-images.push(
-    <img
-      key = "messi"
-      className = "messi"
-      alt = "messi"
-      src = {messi}
-      aria-label = "messi"
-      role = "button"
-      onClick = {displayFact}
-      style = {styleMessi}
-    />
-)
-
-const styleNey = {
-  position : "absolute",
-  top : "180px",
-  right : "350px",
-  width : "200px"
-}
-
-images.push(
-    <img
-      key = "neymar"
-      className = "neymar"
-      alt = "neymar"
-      src = {neymar}
-      aria-label = "neymar"
-      role = "button"
-      onClick = {displayFact}
-      style = {styleNey}
-    />
-)
-
-function displayFact(e){
-  const selectedPlayer = e.target.alt
-  const playerInfo = players[selectedPlayer]
-  const optionIndex = Math.floor(Math.random()*playerInfo.facts.length)
-
-  const funFact = playerInfo.facts[optionIndex]
-  console.log(funFact)
-
-
-  document.getElementById("fact").innerHTML = funFact
-}
-
-const styleFact = {
-  position : "absolute",
-  top : "100px",
-  left : "33%",
-  width : "500px",
-  backgroundColor : "#000",
-  color : "#fff"
-}
-
-const playersFact = 
-  (
-    <div>
-      <h1 style={style_h1}>{title == "" ? "Click on any player" : title}</h1>
-
-      {showBackground && backgroundImg}
-
-      <div className="players">
-        {images}
-      </div>
-
-      <p id="fact" style={styleFact}>
-      
-      </p>
-
-    </div>
-  )
-
-
- 
 function App() {
   return (
-    <div>
-
-    </div>
+   <h1></h1>
   )
 }
-
-ReactDOM.render(playersFact, document.getElementById("react-all"))
 
 export default App;
