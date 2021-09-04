8431256400eac4a68cde07f76e427c40001f1089
shebiraaja <71530161+shebiraaja@users.noreply.github.com>
2021-09-04 12:59:02 +0200

    Add files via upload

---
+++ Data.js
@@ -0,0 +1,46 @@
+export const Data = [
+    {
+        id: 0,
+        name: 'psg',
+        price: 49.99,
+        img: 'https://todosobrecamisetas.com/wp-content/uploads/psg-2019-20-4th-kit-3.jpg',
+    },
+
+    {
+        id: 1,
+        name: 'madrid',
+        price: 49.99,
+        img: 'https://todosobrecamisetas.com/wp-content/uploads/psg-2019-20-4th-kit-3.jpg'
+    },
+
+    {
+        id: 2,
+        name: 'juve',
+        price: 49.99,
+        img: 'https://todosobrecamisetas.com/wp-content/uploads/psg-2019-20-4th-kit-3.jpg'
+    },
+
+    {
+        id: 3,
+        name: 'barça',
+        price: 49.99,
+        img: 'https://todosobrecamisetas.com/wp-content/uploads/psg-2019-20-4th-kit-3.jpg'
+    },
+
+    {
+        id: 4,
+        name: 'city',
+        price: 49.99,
+        img: 'https://todosobrecamisetas.com/wp-content/uploads/psg-2019-20-4th-kit-3.jpg'
+    },
+
+
+    {
+        id: 5,
+        name: 'napoli',
+        price: 49.99,
+        img: 'https://todosobrecamisetas.com/wp-content/uploads/psg-2019-20-4th-kit-3.jpg'
+    },
+
+
+]
